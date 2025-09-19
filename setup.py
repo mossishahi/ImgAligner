f743bf329a6d79b8976c03bd55dee74c2e0827c3
mossishahi <ge65cew@mytum.de>
2025-09-19 11:48:13 +0200

    names updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(name='ImgAligner',
       version='0.1.0',
       description='ImgAligner',
-      author='Mostafa Shahhoseini',
+      author='Mostafa Shahhoseini, Sergio Marco Salas',
       author_email='shahhosseini.94@gmail.com',
       url='https://github.com/mossishahi/ImgAligner',
       packages=['ImgAligner'],
